a62672950e44f03ec6fb57170f8ea7f2a999d407
Andrea <amandacaso97@gmail.com>
2024-11-26 14:54:17 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,8 @@ let cartContent = [];
 
 // CREACIÓN DE FUNCIONES
 
-// Actualizar el total del pedido. Esta función calcula el costo total del carrito sumando los precios de todos los productos, multiplicados por sus cantidades. Luego, actualiza el elemento totalOrderElement en el DOM.
+// *** Actualizar el precio total del pedido. Esta función calcula el costo total del carrito sumando los precios de todos los productos, multiplicados por sus cantidades. Luego, actualiza el elemento totalOrderElement en el DOM. ***
+
 // Reduce: Recorre el array cartContent, acumulando el precio total de los productos
 // toFixed(2): Redondea el número a 2 decimales
 const updateTotalOrderInDOM = () => {
@@ -21,7 +22,7 @@ const updateTotalOrderInDOM = () => {
   totalOrderElement.textContent = '$' + totalOrder.toFixed(2);
 };
 
-// Actualizar la cantidad total de productos. Esta función calcula la cantidad total de productos en el carrito y actualiza el elemento cartQuantityElement. Luego, llama a la función para actualizar el total del pedido.
+// *** Actualizar la cantidad total de productos. Esta función calcula la cantidad total de productos en el carrito y actualiza el elemento cartQuantityElement. Luego, llama a la función para actualizar el total del pedido. ***
 
 const updateProductsQuantityInDOM = () => {
   const totalQuantity = cartContent.reduce((acc, product) => product.quantity + acc, 0);
@@ -30,49 +31,48 @@ const updateProductsQuantityInDOM = () => {
   updateTotalOrderInDOM();
 };
 
-// Actualizar el contenido del carrito. Esta función construye dinámicamente el carrito en el DOM según los productos almacenados en cartContent.
+// *** Actualizar el contenido del carrito. Esta función construye dinámicamente el carrito en el DOM según los productos almacenados en cartContent. ***
 const updateCartInDOM = () => {
   const fragment = document.createDocumentFragment();
 
-  // Si el carrito está vacío, muestra la imagen de "carrito vacío"
   if (cartContent.length === 0) {
-    cartImageElement.classList.remove('hide'); // Muestra la imagen
-    totalOrderContainerElement.classList.add('hide'); // Oculta el total del pedido
+    cartImageElement.classList.remove('hide');
+    totalOrderContainerElement.classList.add('hide');
   } else {
-    cartImageElement.classList.add('hide'); // Oculta la imagen
-    totalOrderContainerElement.classList.remove('hide'); // Muestra el total del pedido
+    cartImageElement.classList.add('hide');
+    totalOrderContainerElement.classList.remove('hide');
   }
 
   // Recorrer los productos del carrito. Se utiliza el método forEach para iterar sobre todos los productos almacenados en el array cartContent
   cartContent.forEach(product => {
     const newCartProduct = document.createElement('div'); // Se crea un <div> que contendrá toda la información de un producto del carrito
-    newCartProduct.classList.add('cart-product'); // Se le asigna la clase cart-product para que tenga estilos predefinidos (probablemente desde el CSS)
+    newCartProduct.classList.add('cart-product'); // Se le asigna la clase cart-product para que tenga estilos predefinidos del CSS
 
     const newCartProductName = document.createElement('p'); //Se crea un <p> para mostrar el nombre del producto.
-    newCartProduct.classList.add('cart-product-name'); // Se le asigna la clase cart-product-name para que tenga los estilos predefinidos del CSS
+    newCartProduct.classList.add('cart-product-name');
     newCartProductName.textContent = product.name; // Así se va a llamar
 
     newCartProduct.append(newCartProductName);
 
     const newCartProductInfo = document.createElement('div'); // Se crea otro <div> para contener los datos adicionales del producto, como la cantidad, precio unitario, precio total y un botón de eliminar.
-    newCartProductInfo.classList.add('cart-product-info'); // Se le asigna la clase cart-product-info para que tenga los estilos predefinidos del CSS
+    newCartProductInfo.classList.add('cart-product-info');
 
     const newCartProductQuantity = document.createElement('span'); // Se crea un <span> para mostrar la cantidad de este producto en el carrito.
-    newCartProductQuantity.classList.add('cart-product-quantity'); // Se le asigna la clase cart-product-info para que tenga los estilos predefinidos del CSS
+    newCartProductQuantity.classList.add('cart-product-quantity');
     newCartProductQuantity.textContent = product.quantity + 'x'; // Se asigna el texto con el formato cantidad x (por ejemplo: 2x)
 
     const newCartProductPriceSingle = document.createElement('span'); // Se crea otro <span> para mostrar el precio unitario del producto, precedido por un símbolo @ (por ejemplo: @10.00).
-    newCartProductPriceSingle.classList.add('cart-product-price-single'); // Se le asigna la clase cart-product-info-single para que tenga los estilos predefinidos del CSS
+    newCartProductPriceSingle.classList.add('cart-product-price-single');
     newCartProductPriceSingle.textContent = '@' + product.price; // Se crea otro <span> para mostrar el precio unitario del producto, precedido por un símbolo @ (por ejemplo: @10.00). ???
 
     const newCartProductPriceTotal = document.createElement('span');
     newCartProductPriceTotal.classList.add('cart-product-price-total');
 
-    // CALCULAR EL PRECIO TOTAL DEL PRODUCTO
+    // Calcular el precio total del producto
     const totalPrice = product.price * product.quantity;
     newCartProductPriceTotal.textContent = '$' + totalPrice.toFixed(2);
 
-    // CREAR BOTON CON IMG PARA ELIMINAR EL PRODUCTO (PRODUCT.NAME)
+    // Crear botón con img para eliminar el producto (product.name)
     const newCartProductIconRemove = document.createElement('img'); // Se crea un elemento <img> que servirá como botón para eliminar el producto.
     newCartProductIconRemove.classList.add('cart-product-icon-remove');
     newCartProductIconRemove.src = './assets/images/icon-remove-item.svg'; // Se asigna una imagen a través de su atributo src.
@@ -92,8 +92,40 @@ const updateCartInDOM = () => {
     fragment.append(newCartProduct);
   });
 
-  // ACTUALIZAR EL CONTENEDOR DEL CARRITO
+  // Actualizar el contenedor del carrito
   cartProductsElement.textContent = ''; // Con esta línea vaciamos el contenedor antes de agregar nuevos elementos. Si no se hace los productos anteriores se seguirían mostrando
   cartProductsElement.append(fragment);
   updateProductsQuantityInDOM();
 };
+
+const updateProductQuantityInDOM = (quantity, element) => {
+  element.children[1].textContent = quantity;
+  updateCartInDOM();
+};
+
+const addProductToCart = (name, price) => {
+  cartContent.push({ name: name, price: price, quantity: 1 });
+  updateCartInDOM();
+};
+
+// *** Activar los filtros ***
+
+/* Recordemos: 
+
+- event.target: HTML que se ha clicado. Este puede ser un botón, un enlace o cualquier otro elemento que tenga un evento asociado
+
+- dataset: Es una propiedad que nos permite acceder a los atributos data-* definidos en el elemento HTML. */
+
+const activateFilters = event => {
+  const filter = event.target.dataset.filter;
+
+  if (!filter) return;
+
+  for (const filter of filtersElement.children) {
+    // Aquí, estamos iterando sobre todos los elementos hijos de filtersElement, que es el contenedor que contiene los filtros
+    filter.classList.remove('filter-active');
+  }
+  event.target.classList.add('filter-active');
+};
+
+filtersElement.addEventListener('click', activateFilters);
